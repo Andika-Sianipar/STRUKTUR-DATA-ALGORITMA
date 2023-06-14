1d2920032d15f250de3cf19cf1370574f13e4c7d
Dikzz <120103574+Andika-Sianipar@users.noreply.github.com>
2023-06-14 22:23:34 +0800

    Update main.cpp

--- BinaryTree-Project/main.cpp
+++ BinaryTree-Project/main.cpp
@@ -1,9 +1,6 @@
 #include <iostream>
 #include "Binary_Tree.h"
 
-
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
 int main(int argc, char** argv) {
 	BinaryTree bin;
 	
